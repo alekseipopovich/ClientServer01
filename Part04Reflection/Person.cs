14ed193fb291ad3bf9b561f70724bf2e307b688e
alekseipopovich <aleksei@popovichab.ru>
2025-04-03 15:51:41 +1000

    reflection added

---
+++ Part04Reflection/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Part04Reflection
+{
+    public class Person
+    {
+        string name;
+        public int Age { get; set; }
+
+        public Person(string name) => this.name = name;
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.Age = age;
+        }
+        public void Print() => Console.WriteLine($"Name: {name} Age: {Age}");
+    }
+
+}
